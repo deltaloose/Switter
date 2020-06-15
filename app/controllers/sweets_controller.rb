8c0dce03962c5452e39cf132fdd7cc991a298da3
vagrant <vagrant@localhost.localdomain>
2020-06-15 13:31:15 +0000

    add_controller

---
+++ app/controllers/sweets_controller.rb
@@ -0,0 +1,16 @@
+class SweetsController < ApplicationController
+  #おかし新規登録ページの表示
+  def new
+  end
+  #おかしの新規作成
+  def create
+  end
+  #おかし一覧
+  def index
+  end
+  #おかし詳細
+  def show
+  end
+
+  #おかし投稿時のストロングパラメータ
+end
