f927b55f688b4b4399f1bdf64df5a323936acbe9
vagrant <vagrant@localhost.localdomain>
2020-06-16 08:42:11 +0000

    fix

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -5,7 +5,6 @@ class SweetsController < ApplicationController
   end
   #おかしの新規作成
   def create
-    byebug
     sweet = current_user.sweets.new(sweet_params)
     #@sweet.user_id = current_user.id
     sweet.save
