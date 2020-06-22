93091250b194bb2b4d897bd6f27e0fac4561e7b8
vagrant <vagrant@localhost.localdomain>
2020-06-22 00:38:37 +0000

    add_routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sweets#index'
   resources :users, only: [:show, :index, :edit, :update]
-  resources :sweets, only: [:new, :create, :index, :show, :edit, :update]
+  resources :sweets, only: [:new, :create, :index, :show, :edit, :update] do
+    resources :sweet_comments, only: [:create, :destroy]
+  end
 end
